46b31ef95666188ed4e6e1a5d9a9f7a971a13b1b
Francis Wang <frwang@mit.edu>
2021-03-07 19:16:23 -0500

    Chisel mem module

--- cache/ch/src/main/scala/cache/Mem.scala
+++ cache/ch/src/main/scala/cache/Mem.scala
@@ -8,12 +8,25 @@ class Mem extends Module {
 
   val io = IO(new MemBundle)
 
-  val mem = Reg(Vec(1 << Const.ADDR_WIDTH, UInt(Const.DATA_WIDTH.W)))
+  val mem = RegInit(VecInit(Seq.fill(1 << Const.ADDR_WIDTH)(0.U(Const.DATA_WIDTH.W))))
+  val data = Reg(UInt(Const.DATA_WIDTH.W))
+  val valid = RegInit(false.B)
 
-  io.rspValid := false.B
-  io.rspData := 0.U
+  io.rspData := RegNext(data)
+  io.rspValid := RegNext(valid)
 
   when (io.reqOp =/= Const.op_invalid) {
-    printf("mem received op=%x", io.reqOp)
+    printf("mem received op=%d addr=0x%x data=0x%x\n", io.reqOp, io.reqAddr, io.reqData)
+    when (io.reqOp === Const.op_write) {
+      mem(io.reqAddr) := io.reqData
+      data := 0.U
+      valid := false.B
+    } .otherwise {
+      data := mem(io.reqAddr)
+      valid := true.B
+    }
+  } .otherwise {
+    data := 0.U
+    valid := false.B
   }
 }
--- cache/ch/src/test/scala/cache/CacheSpec.scala
+++ cache/ch/src/test/scala/cache/CacheSpec.scala
@@ -4,7 +4,49 @@ package cache
 
 import chisel3.iotesters._
 
-class CacheTester(dut: Top) extends PeekPokeTester(dut)
+import scala.util.Random
+
+class CacheTester(dut: Top) extends PeekPokeTester(dut) {
+
+  val mem = new Array[Int](1 << Const.ADDR_WIDTH)
+
+  for (_ <- 0 until 100) {
+    if (Random.nextBoolean()) {
+      // write mem
+      val addr = Random.nextInt() & ((1 << Const.ADDR_WIDTH) - 1)
+      val data = Random.nextInt() & ((1 << Const.DATA_WIDTH) - 1)
+      mem(addr) = data
+      poke(dut.io.reqOp, Const.op_write)
+      poke(dut.io.reqAddr, addr)
+      poke(dut.io.reqData, data)
+      printf("tb write addr=0x%x data=0x%x\n", addr, data)
+      step(1)
+
+      poke(dut.io.reqOp, Const.op_invalid)
+      step(1)
+    } else {
+      // read mem
+      val addr = Random.nextInt() & ((1 << Const.ADDR_WIDTH) - 1)
+      poke(dut.io.reqOp, Const.op_read)
+      poke(dut.io.reqAddr, addr)
+      printf("tb read addr=0x%x\n", addr)
+      step(1)
+
+      poke(dut.io.reqOp, Const.op_invalid)
+      step(1)
+
+      while (peek(dut.io.rspValid) != BigInt(1)) step(1)
+      val data = peek(dut.io.rspData)
+      val expected = mem(addr)
+
+      if (data == expected) {
+        printf("tb PASS data=0x%x expected=0x%x\n", data, expected)
+      } else {
+        printf("tb FAIL data=0x%x expected=0x%x\n", data, expected)
+      }
+    }
+  }
+}
 
 class CacheSpec extends ChiselFlatSpec {
   behavior of "Top"
