900189fb66e92511e3e48a2b2c128c8caa9e6c61
Francis Wang <frwang@mit.edu>
2021-03-16 11:06:03 -0400

    Verik alu working

--- alu/vk/src/main/kotlin/Alu.kt
+++ alu/vk/src/main/kotlin/Alu.kt
@@ -1,4 +1,5 @@
 import verik.base.*
+import verik.data.*
 
 class Alu: Module() {
 
@@ -6,4 +7,21 @@ class Alu: Module() {
     @input  var a   = t_UbitData()
     @input  var b   = t_UbitData()
     @output var x   = t_UbitData()
+
+    @com fun update() {
+        @Suppress("LiftReturnOrAssignment")
+        // TODO support exhaustive case assignment
+        when (op) {
+            Op.ADD -> x = a + b
+            Op.SUB -> x = a - b
+            Op.AND -> x = a and b
+            Op.OR -> x = a or b
+            Op.XOR -> x = a xor b
+            Op.SLT -> x = if (s(a) < s(b)) u(DATA_WIDTH, 1) else u(DATA_WIDTH, 0)
+            Op.SLTU -> x = if (a < b) u(DATA_WIDTH, 1) else u(DATA_WIDTH, 0)
+            Op.SLL -> x = a shl b
+            Op.SRL -> x = a shr b
+            Op.SRA -> x = u(s(a) shr b)
+        }
+    }
 }
--- alu/vk/src/main/kotlin/AluTb.kt
+++ alu/vk/src/main/kotlin/AluTb.kt
@@ -1,4 +1,5 @@
 import verik.base.*
+import verik.data.*
 
 class AluTb: Module() {
 
@@ -7,22 +8,63 @@ class AluTb: Module() {
     @output var b   = t_UbitData()
     @input  var x   = t_UbitData()
 
+    var expected = t_UbitData()
+
     @run fun test() {
         repeat(200) { transact() }
     }
 
     @task fun transact() {
-        op = when (random(10)) {
-            0 -> Op.ADD
-            1 -> Op.SUB
-            2 -> Op.AND
-            3 -> Op.OR
-            4 -> Op.XOR
-            5 -> Op.SLT
-            6 -> Op.SLTU
-            7 -> Op.SLL
-            8 -> Op.SRL
-            else -> Op.SRA
+        a = u(DATA_WIDTH, random())
+        b = u(DATA_WIDTH, random())
+        @Suppress("MoveVariableDeclarationIntoWhen")
+        // TODO support inlined variable
+        // TODO support random enum
+        val s = random(10)
+        when(s) {
+            0 -> {
+                op = Op.ADD
+                expected = a + b
+            }
+            1 -> {
+                op = Op.SUB
+                expected = a - b
+            }
+            2 -> {
+                op = Op.AND
+                expected = a and b
+            }
+            3 -> {
+                op = Op.OR
+                expected = a or b
+            }
+            4 -> {
+                op = Op.XOR
+                expected = a xor b
+            }
+            5 -> {
+                op = Op.SLT
+                expected = if (s(a) < s(b)) u(DATA_WIDTH, 1) else u(DATA_WIDTH, 0)
+            }
+            6 -> {
+                op = Op.SLTU
+                expected = if (a < b) u(DATA_WIDTH, 1) else u(DATA_WIDTH, 0)
+            }
+            7 -> {
+                op = Op.SLL
+                expected = a shl b
+            }
+            8 -> {
+                op = Op.SRL
+                expected = a shr b
+            }
+            else -> {
+                op = Op.SRA
+                expected = u(s(a) shr b)
+            }
         }
+        delay(1)
+        if (x == expected) print("PASS") else print("FAIL")
+        println(" op=$op a=0x$a b=0x$b x=0x$x expected=0x$expected")
     }
 }
